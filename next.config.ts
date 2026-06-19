a86e9019581cb9811c38f5b5721331cf942d20b5
vinay.nain <vinay.nain@mastersunion.org>
2026-06-19 13:45:28 +0530

    update next config

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,16 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   typescript: {
-    // ⚠️ WARNING: Allows build to succeed even with TypeScript errors
     ignoreBuildErrors: true,
   },
-  // `eslint: { ignoreDuringBuilds: true }` is no longer supported in Next.js 16
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.mastersunion.link",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
